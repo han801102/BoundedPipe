1eab8a17b1e83bb4ed30061c658293e869d58c94
Han <pohan@vt.edu>
2021-03-09 22:37:41 -0500

    Refactor test

--- test/boundedpipe/CircArrayPipeTest.java
+++ test/boundedpipe/CircArrayPipeTest.java
@@ -1,9 +1,9 @@
 package boundedpipe;
 
-public class ListPipeTest extends BasePipeTest {
+public class CircArrayPipeTest extends BasePipeTest {
     @Override
     public Pipe<String> initPipe(int capacity, String... args) {
-        Pipe<String> p = new ListPipe<>(capacity);
+        Pipe<String> p = new CircArrayPipe<>(capacity);
         for (String s : args) {
             p.append(s);
         }
